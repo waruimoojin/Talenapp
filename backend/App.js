393fd13ea505a84db886bab701f0fc5379dda2a1
root <root@vmtest>
2024-09-15 18:11:58 +0000

    change api ip

--- backend/App.js
+++ backend/App.js
@@ -9,7 +9,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://192.168.56.13:5173"],
         methods: ["GET,POST,DELETE,PUT,PATCH"],
         credentials: true,
     })
